0062ced42625dae0d57629a7782cfa6f2e986f0f
prabina.khanal@introcept.co <prabina.khanal@introcept.co>
2022-06-06 22:21:25 +0545

    created closing effect while clicking outside the div or in blankspace

--- modal.js
+++ modal.js
@@ -3,11 +3,12 @@ const modal=document.querySelector('#modal');
 const close=document.querySelector('#close');
 const clickModal=document.querySelector('#clickmodal');
 const cross=document.querySelector('#cross');
+const all=document.querySelector('#all');
+
 
 
 
 open.addEventListener('click',() =>{
-    
     modal.classList.remove('hidden');
 clickModal.classList.add('hidden')
 })
@@ -24,3 +25,10 @@ cross.addEventListener('click',() =>{
     modal.classList.add('hidden');
     clickModal.classList.remove('hidden')
 })
+
+modal.addEventListener('click',()=>{
+    modal.classList.add('hidden');
+    clickModal.classList.remove('hidden')
+
+})
+
